41feb058ba78989c0e06e24c73bd4f098b3ca15b
Giang Binh Phan <phan.phan@hitachivantara.com>
2022-08-26 11:45:25 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { 
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute
+} from './pages'
+import { Navbar, Sidebar, Footer } from './components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Sidebar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/about' element={<About/>}/>
+        <Route exact path='/cart' element={<Cart/>}/>
+        <Route exact path='/products' element={<Products/>}/>
+        <Route exact path='/products/:id' element={<SingleProduct/>}/>
+        <Route exact path='/checkout' element={<Checkout/>}/>
+        <Route exact path='*' element={<Error/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import Home from "./HomePage";
+import PrivateRoute from "./PrivateRoute";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import About from "./AboutPage";
+
+export {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+}
