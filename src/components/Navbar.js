95b028bbd255502e5d0596f6eec37447e2d9015c
Giang Binh Phan <phan.phan@hitachivantara.com>
2022-08-29 17:29:21 +0700

    NavBar Full

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
 
 import logo from '../assets/logo.svg';
+import { links } from '../utils/constants';
 
 const Navbar = () => {
   return (
@@ -13,10 +14,20 @@ const Navbar = () => {
           <Link to="/">
             <img src={logo} alt="comfy sloth" />
           </Link>
-          <button className="nav-toggle" type="button">
+          <button type="button" className="nav-toggle">
             <FaBars />
           </button>
         </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            const { id, text, url } = link;
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </NavContainer>
   );
@@ -51,6 +62,41 @@ const NavContainer = styled.nav`
       font-size: 2rem;
     }
   }
+  .nav-links {
+    display: none;
+  }
+  .cart-btn-wrapper {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .nav-toggle {
+      display: none;
+    }
+    .nav-center {
+      display: grid;
+      grid-template-columns: auto 1fr auto;
+      align-items: center;
+    }
+    .nav-links {
+      display: flex;
+      justify-content: center;
+      li {
+        margin: 0 0.5rem;
+      }
+      a {
+        color: var(--clr-grey-3);
+        font-size: 1rem;
+        text-transform: capitalize;
+        letter-spacing: var(--spacing);
+        padding: 0.5rem;
+        &:hover {
+          border-bottom: 2px solid var(--clr-primary-7);
+        }
+      }
+    }
+    .cart-btn-wrapper {
+      display: grid;
+    }
+  }
 `;
-
 export default Navbar;
